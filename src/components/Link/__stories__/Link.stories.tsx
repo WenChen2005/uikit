9dedee77eab6686bfef8c962b3933899f1600c4c
WenChen2005 <wchen5@ufl.edu>
2025-09-23 23:25:50 -0400

    feat(link): added view story showing normal, primary, secondary

--- src/components/Link/__stories__/Link.stories.tsx
+++ src/components/Link/__stories__/Link.stories.tsx
@@ -22,8 +22,26 @@ export default {
     },
 } as Meta;
 
+// Default story
 const DefaultTemplate: StoryFn<LinkProps> = (args) => <Link {...args}>Link</Link>;
 export const Default = DefaultTemplate.bind({});
 
+// Showcase story
 const ShowcaseTemplate: StoryFn = () => <LinkShowcase />;
 export const Showcase = ShowcaseTemplate.bind({});
+
+// View story
+const ViewTemplate: StoryFn<LinkProps> = (args) => (
+    <div style={{display: 'flex', gap: '1rem'}}>
+        <Link {...args} view="normal">
+            Normal
+        </Link>
+        <Link {...args} view="primary">
+            Primary
+        </Link>
+        <Link {...args} view="secondary">
+            Secondary
+        </Link>
+    </div>
+);
+export const View = ViewTemplate.bind({});
